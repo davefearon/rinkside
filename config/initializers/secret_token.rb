c87837d352f27b2973e1dac248e108bee2b33ccd
Dave Fearon <dave.fearon@gmail.com>
2010-12-14 14:53:31 -0500

    The very very base of the project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rinkside::Application.config.secret_token = 'e31d14f58c38bb4f2520229330d00b0138cf99f4bc2c28dd88ad8ea7b2630bbb8bcd72d428ea16b079c2bdcf66b8b87aaff8a5635e61b0ba18eeffd5c2bff228'
