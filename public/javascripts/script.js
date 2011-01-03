24a2a60f71a809fd0f8b2de32e7d696b9df44a19
Warren Dunlop <warrendunlop@gmail.com>
2011-01-03 05:29:18 -0500

    Lots and lots of tweaks.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,18 +1,13 @@
-/* Author: 
-
-*/
-
 $(document).ready(function(){
     $(window).bind("resize", function(){
         init();
-        init();
     });
     init();
-    init();
+
     function init() {
         $('#map_canvas').css({
-            width: $(window).width(),
-            height: $(window).height()// - $('#header').height() - $('#footer').height()
+            width: $(window).width() - 200,
+            height: $(window).height()
         });
     }
 });
