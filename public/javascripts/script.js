07e680500af70e1a6b2e1c48f20237f887508a3b
Dave Fearon <dave.fearon@gmail.com>
2011-01-04 13:57:48 -0500

    fixed?

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,19 +1,21 @@
 $(document).ready(function(){
-    $(window).bind("resize", function() {
-      init();
-      init();
-    });
+  $(window).bind("resize", function() {
     init();
     init();
-    init();
-
-    function init() {
-      $('#map_canvas').css({
-          width: $(window).width() - 200,
-          height: $(window).height()
-      });
-      $('#header').css({
-        height: $(window).height() - $('#logo').height() - $('#footer').height() - 60
-      });
-    }
+  });
+  init();
+  init();
+  timer = setTimeout("init()",500);
 });
+
+function init() {
+  console.debug('hit');
+  $('#map_canvas').css({
+      width: $(window).width() - 200,
+      height: $(window).height()
+  });
+  $('#header').css({
+    height: $(window).height() - $('#logo').height() - $('#footer').height() - 60
+  });
+  return true;
+}
