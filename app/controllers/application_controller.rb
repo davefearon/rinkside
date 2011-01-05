6fc06783b2ab1c38cf740bdb4cbbcfff295d4957
Dave Fearon <dave.fearon@gmail.com>
2011-01-05 13:45:16 -0500

    webOS devices are a pain in the @$$

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   layout :detect_browser
 
   private
-  MOBILE_BROWSERS = ["android", "ipod", "iphone", "ipad", "opera mini", "blackberry", "palm", "hiptop", "avantgo", "plucker", "xiino", "blazer", "elaine", "windows ce; ppc;", "windows ce; smartphone;", "windows ce; iemobile", "up.browser", "up.link", "mmp", "symbian", "smartphone", "midp", "wap", "vodafone", "o2", "pocket", "kindle", "mobile", "pda", "psp", "treo"]
+  MOBILE_BROWSERS = ["android", "ipod", "iphone", "ipad", "opera mini", "blackberry", "webOS", "hiptop", "avantgo", "plucker", "xiino", "blazer", "elaine", "windows ce; ppc;", "windows ce; smartphone;", "windows ce; iemobile", "up.browser", "up.link", "mmp", "symbian", "smartphone", "midp", "wap", "vodafone", "o2", "pocket", "kindle", "mobile", "pda", "psp", "treo"]
 
   def detect_browser
     layout = selected_layout
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     agent = request.headers["HTTP_USER_AGENT"].downcase
     MOBILE_BROWSERS.each do |m|
       if agent.match(m)
-        if m == "palm"
+        if m == "webOS"
           return "iphone"
         end
         return m
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
     session.inspect
     if session.has_key? "layout"
       return "iphone" if (session["layout"] == "iphone")
-      return "iphone" if (session["layout"] == "palm")
+      return "iphone" if (session["layout"] == "webOS")
       return "ipad" if (session["layout"] == "ipad")
       return "ipod" if (session["layout"] == "ipod")
       return "application"
